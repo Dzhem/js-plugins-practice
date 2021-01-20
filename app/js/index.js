5adb4b920f57e8df6decb61de630297844152959
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2021-01-20 14:26:10 +0300

    added the ability to create footer buttons

--- app/js/index.js
+++ app/js/index.js
@@ -4,7 +4,17 @@ const myModal = $.modal({
         <p>Это окошко прекрасно!</p>
     `,
     closable: true,
-    width: '400px'
+    width: '400px',
+    footerButtons: [
+        {text: 'Ok', type: 'primary', handler() {
+            console.log('Primary btn clocked')
+            myModal.close()
+        }},
+        {text: 'Cancel', type: 'danger', handler() {
+            console.log('Danger btn clocked')
+            myModal.close()
+        }}
+    ]
 })
 
 myModal.open()
