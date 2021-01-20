75ed6502120eb46c3208b3b5016744415a5c78a9
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2021-01-20 21:51:44 +0300

    add the cards

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,8 @@
+const fruits = [
+    {id: 1, title: 'Яблоки', price: 20, img: 'https://graalopt.ru/wp-content/uploads/2020/07/yabloko-krasnoe.jpg'},
+    {id: 1, title: 'Апельсины', price: 60, img: 'https://png.rinvik.ru/files/Tri-apelsina-s-dvumya-dolkami-i-listyami.png'},
+    {id: 1, title: 'Манго', price: 80, img: 'https://sc01.alicdn.com/kf/UTB8ADJqXJnJXKJkSaiyq6AhwXXae/736416451/UTB8ADJqXJnJXKJkSaiyq6AhwXXae.jpg'}
+]
 const myModal = $.modal({
     title: 'Заголовок',
     content: `
